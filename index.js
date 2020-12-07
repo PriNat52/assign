750a60d2b74cfdeeb743ce7cf04b40e1b26975a9
PriNat52 <75464396+PriNat52@users.noreply.github.com>
2020-12-06 21:41:25 -0500

    Assignment_5

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
+/*  Trying out jQuery and Javascript */
 
 $(document).ready(function(){
   $('#button1').click(function(){
+    console.log("hello");
     makeRequest();
    });
-  });
+ });
 
-  function makeRequest(){
-    let httpRequest = new XMLHttpRequest();
-    if(!httpRequest){
-      alert("Exit: Records cannot be found");
-      return false;
-    }
-    httpRequest.onreadystatechange = function (){
-      if(httpRequest.readystate == XMLHttpRequest.DONE){
-        if (httpRequest.status === 200){
-          document.write(httpRequest.responseText);
-        }else{
-          alert("Found an Error with data.")
-        }
-      }
-    };
-    httpRequest.open('GET','my_studies.json');
-    httpRequest.send();
+ function makeRequest(){
+   
+  let httpRequest = new XMLHttpRequest();
+   
+  if(!httpRequest){
+    console.log("Exit: Records cannot be found");
+    return false;
   }
+   
+   httpRequest.onreadystatechange = function() {
+     if(httpRequest.readyState === XMLHttpRequest.DONE){
+       
+       console.log('in function');
+       if (httpRequest.status === 200){
+         
+         console.log('inside 200');
+         let myObj,txt="",x=0;
+         myObj = JSON.parse(httpRequest.responseText).my_studies;
+         txt += "<table border='1'>"
+         for (x in myObj) {
+           txt += "<tr><td>" + myObj[x].study.Name + "</td><td>"+ myObj[x].study.Major + "</td><td>" + myObj[x].study.Course + "</td><td>" + myObj[x].study.Year +"</td></tr>";
+          }
+        txt += "</table>"    
+        document.getElementById("result").innerHTML = txt;
+         
+         }else{
+           console.log('error at 200');
+           alert("Error: at 200");
+        }
+       }else{
+         console.log("problem with ready state")
+       }
+   };
+   
+   httpRequest.open('GET','my_studies.json');
+   httpRequest.send();
+}
